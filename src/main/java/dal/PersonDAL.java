1d2a74ab56f80a83de25f3b20296da15cacaafcf
raspy-on-osu <email.jack.avery@gmail.com>
2021-08-08 15:11:46 -0400

    Actually completes Person

---
+++ src/main/java/dal/PersonDAL.java
@@ -0,0 +1,59 @@
+package dal;
+
+import entity.Person;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author Jack Avery
+ */
+public class PersonDAL extends GenericDAL<Person> {
+    
+    public PersonDAL() {
+        super(Person.class);
+    }
+    
+    @Override
+    public List<Person> findAll() {
+        return findResults("Person.findAll", null);
+    }
+    
+    @Override
+    public Person findById(int personId) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personId", personId);
+        return findResult("Person.findByPersonId", map);
+    }
+    
+    public List<Person> findByFirstName(String firstName) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personFirstName", firstName);
+        return findResults("Person.findByFirstName", map);
+    }
+    
+    public List<Person> findByLastName(String lastName) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personLastName", lastName);
+        return findResults("Person.findByLastName", map);
+    }
+    
+    public List<Person> findByPhone(String phone) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personPhone", phone);
+        return findResults("Person.findByPhone", map);
+    }
+    
+    public List<Person> findByAddress(String address) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personAddress", address);
+        return findResults("Person.findByAddress", map);
+    }
+    
+    public List<Person> findByBirth(Date birth) {
+        Map<String, Object> map = new HashMap<>();
+        map.put( "personBirth", birth);
+        return findResults("Person.findByBirth", map);
+    }
+}
---
+++ src/main/java/logic/PersonLogic.java
@@ -0,0 +1,170 @@
+package logic;
+
+import common.ValidationException;
+import dal.DonationRecordDAL;
+import dal.PersonDAL;
+import entity.BloodBank;
+import entity.DonationRecord;
+import entity.Person;
+import entity.RhesusFactor;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import java.util.List;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+import java.util.function.ObjIntConsumer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author Jack Avery
+ */
+public class PersonLogic extends GenericLogic<Person, PersonDAL>  {
+    
+    public static final String FIRST_NAME = "first_name";
+    public static final String LAST_NAME = "last_name";
+    public static final String PHONE = "phone";
+    public static final String ADDRESS = "address";
+    public static final String BIRTH = "birth";
+    public static final String BLOODBANK_ID = "bloodbank_id";
+    public static final String ID = "id";
+    
+    PersonLogic() {
+        super(new PersonDAL());
+    }
+
+    @Override
+    public List<Person> getAll() {
+        return get(() -> dal().findAll());
+    }
+
+    @Override
+    public Person getWithId(int id) {
+        return get(() -> dal().findById(id));
+    }
+    
+    public List<Person> getPersonWithFirstName(String firstName) {
+        return get(() -> dal().findByFirstName(firstName));
+    }
+    
+    public List<Person> getPersonWithLastName(String lastName) {
+        return get(() -> dal().findByLastName(lastName));
+    }
+    
+    public List<Person> getPersonWithPhone(String phone) {
+        return get(() -> dal().findByPhone(phone));
+    }
+    
+    public List<Person> getPersonWithAddress(String address) {
+        return get(() -> dal().findByAddress(address));
+    }
+    
+    public List<Person> getPersonWithBirth(Date birth) {
+        return get(() -> dal().findByBirth(birth));
+    }
+    
+    @Override
+    public Person createEntity(Map<String, String[]> parameterMap) {
+        Objects.requireNonNull(parameterMap, "parameterMap cannot be null");
+
+        Person entity = new Person();
+
+        if (parameterMap.containsKey(ID)) {
+            try {
+                entity.setId(Integer.parseInt(parameterMap.get(ID)[0]));
+            } catch (java.lang.NumberFormatException ex) {
+                throw new ValidationException(ex);
+            }
+        }
+
+        ObjIntConsumer< String> validator = (value, length) -> {
+            if (value == null || value.trim().isEmpty() || value.length() > length) {
+                String error = "";
+                if (value == null || value.trim().isEmpty()) {
+                    error = "value cannot be null or empty: " + value;
+                }
+                if (value.length() > length) {
+                    error = "string length is " + value.length() + " > " + length;
+                }
+                throw new ValidationException(error);
+            }
+        };
+
+        String firstName = parameterMap.get(FIRST_NAME)[0];
+        String lastName = parameterMap.get(LAST_NAME)[0];
+        String phone = parameterMap.get(PHONE)[0];
+        String address = parameterMap.get(ADDRESS)[0];
+        String birth = parameterMap.get(BIRTH)[0];
+        
+        validator.accept(firstName, 50);
+        validator.accept(lastName, 50);
+        validator.accept(phone, 15);
+        validator.accept(address, 100);
+        validator.accept(birth, 45);
+        
+        entity.setFirstName(firstName);
+        entity.setLastName(lastName);
+        entity.setPhone(phone);
+        entity.setAddress(address);
+        
+        Date dateCreated = null;
+        try {
+            dateCreated = new SimpleDateFormat("yyyy-MM-dd").parse(birth);
+        } catch (ParseException ex) {
+            Logger.getLogger(PersonLogic.class.getName()).log(Level.SEVERE, null, ex);
+            throw new ValidationException("failed to format String=\"" + birth + "\" to a date object" + dateCreated, ex);
+        }
+        entity.setBirth(dateCreated);
+        
+        if(parameterMap.containsKey(BLOODBANK_ID)) {
+            try {
+                String bloodbankId = parameterMap.get(BLOODBANK_ID)[0];
+                validator.accept(bloodbankId, 10);
+                entity.setBloodBank(new BloodBankDAL().findById(Integer.parseInt(bloodbankId)));
+            } catch(java.lang.NumberFormatException ex) {
+                throw new ValidationException(ex);
+            }
+        }
+        
+        List<DonationRecord> donations;
+        if(entity.getId() != null) {
+            donations = (ArrayList) new DonationRecordDAL().findByPerson(entity.getId());
+            
+            if(donations.size() > 0) {
+                Set<DonationRecord> donationSet = Set.copyOf(donations);
+                entity.setDonationRecordSet(donationSet);
+            }
+        }
+        
+        return entity;
+    }
+    
+    @Override
+    public List<String> getColumnNames() {
+        return Arrays.asList("ID", "First Name", "Last Name", "Phone Number", "Address", "Date of Birth");
+    }
+
+    @Override
+    public List<String> getColumnCodes() {
+        return Arrays.asList(ID, FIRST_NAME, LAST_NAME, PHONE, ADDRESS, BIRTH);
+    }
+
+    @Override
+    public List<?> extractDataAsList(Person e) {
+        return Arrays.asList(
+                e.getId(),
+                e.getFirstName(),
+                e.getLastName(),
+                e.getPhone(),
+                e.getAddress(),
+                e.getBirth(),
+                e.getBloodBank(),
+                e.getDonationRecordSet());
+    }
+    
+}
