d1394a89060f1d817ef15a9326265ab084adcdc4
aver0042 <84288027+aver0042@users.noreply.github.com>
2021-08-13 16:12:22 -0400

    Completes PersonLogicTest

--- src/test/java/logic/PersonLogicTest.java
+++ src/test/java/logic/PersonLogicTest.java
@@ -2,6 +2,7 @@ package logic;
 
 import common.EMFactory;
 import common.TomcatStartUp;
+import common.ValidationException;
 import entity.BloodBank;
 import entity.DonationRecord;
 import entity.Person;
@@ -10,12 +11,16 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Set;
+import java.util.function.Consumer;
+import java.util.function.IntFunction;
 import javax.persistence.EntityManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -235,6 +240,149 @@ public class PersonLogicTest {
         returnedPerson.setDonationRecordSet(expectedEntity.getDonationRecordSet());
 
         assertPersonEquals(expectedEntity, returnedPerson);
+    }
+    
+    @Test
+    final void testCreateEntityNullAndEmptyValues() {
+        Map<String, String[]> sampleMap = new HashMap<>();
+        Consumer<Map<String, String[]>> fillMap = (Map<String, String[]> map) -> {
+            map.clear();
+            map.put(PersonLogic.ID, new String[]{Integer.toString(expectedEntity.getId())});
+            map.put(PersonLogic.FIRST_NAME, new String[]{expectedEntity.getFirstName()});
+            map.put(PersonLogic.LAST_NAME, new String[]{expectedEntity.getLastName()});
+            map.put(PersonLogic.PHONE, new String[]{expectedEntity.getPhone()});
+            map.put(PersonLogic.ADDRESS, new String[]{expectedEntity.getAddress()});
+            map.put(PersonLogic.BIRTH, new String[]{logic.convertDateToString(expectedEntity.getBirth())});
+        };
+
+        //idealy every test should be in its own method
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.ID, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.ID, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.FIRST_NAME, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.FIRST_NAME, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.LAST_NAME, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.LAST_NAME, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.PHONE, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.PHONE, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.ADDRESS, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.ADDRESS, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.BIRTH, null);
+        assertThrows(NullPointerException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.BIRTH, new String[]{});
+        assertThrows(IndexOutOfBoundsException.class, () -> logic.createEntity(sampleMap));
+    }
+    
+    @Test
+    final void testCreateEntityBadLengthValues() {
+        Map<String, String[]> sampleMap = new HashMap<>();
+        Consumer<Map<String, String[]>> fillMap = (Map<String, String[]> map) -> {
+            map.clear();
+            map.put(PersonLogic.ID, new String[]{Integer.toString(expectedEntity.getId())});
+            map.put(PersonLogic.FIRST_NAME, new String[]{expectedEntity.getFirstName()});
+            map.put(PersonLogic.LAST_NAME, new String[]{expectedEntity.getLastName()});
+            map.put(PersonLogic.PHONE, new String[]{expectedEntity.getPhone()});
+            map.put(PersonLogic.ADDRESS, new String[]{expectedEntity.getAddress()});
+            map.put(PersonLogic.BIRTH, new String[]{logic.convertDateToString(expectedEntity.getBirth())});
+        };
+
+        IntFunction<String> generateString = (int length) -> {
+            //https://www.baeldung.com/java-random-string#java8-alphabetic
+            //from 97 inclusive to 123 exclusive
+            return new Random().ints('a', 'z' + 1).limit(length)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+        };
+
+        //idealy every test should be in its own method
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.FIRST_NAME, new String[]{""});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.FIRST_NAME, new String[]{generateString.apply(51)});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.LAST_NAME, new String[]{""});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.LAST_NAME, new String[]{generateString.apply(51)});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.PHONE, new String[]{""});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.PHONE, new String[]{generateString.apply(16)});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        
+        fillMap.accept(sampleMap);
+        sampleMap.replace(PersonLogic.ADDRESS, new String[]{""});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+        sampleMap.replace(PersonLogic.ADDRESS, new String[]{generateString.apply(101)});
+        assertThrows(ValidationException.class, () -> logic.createEntity(sampleMap));
+    }
+    
+    
+    @Test
+    final void testCreateEntityEdgeValues() {
+        IntFunction<String> generateString = (int length) -> {
+            //https://www.baeldung.com/java-random-string#java8-alphabetic
+            return new Random().ints('a', 'z' + 1).limit(length)
+                    .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                    .toString();
+        };
+
+        Map<String, String[]> sampleMap = new HashMap<>();
+        sampleMap.put(PersonLogic.ID, new String[]{Integer.toString(expectedEntity.getId())});
+        sampleMap.put(PersonLogic.FIRST_NAME, new String[]{expectedEntity.getFirstName()});
+        sampleMap.put(PersonLogic.LAST_NAME, new String[]{expectedEntity.getLastName()});
+        sampleMap.put(PersonLogic.PHONE, new String[]{expectedEntity.getPhone()});
+        sampleMap.put(PersonLogic.ADDRESS, new String[]{expectedEntity.getAddress()});
+        sampleMap.put(PersonLogic.BIRTH, new String[]{logic.convertDateToString(expectedEntity.getBirth())});
+
+        //idealy every test should be in its own method
+        Person returnedPerson = logic.createEntity(sampleMap);
+        assertEquals(Integer.parseInt(sampleMap.get(PersonLogic.ID)[0]), returnedPerson.getId());
+        assertEquals(sampleMap.get(PersonLogic.FIRST_NAME)[0], returnedPerson.getFirstName());
+        assertEquals(sampleMap.get(PersonLogic.LAST_NAME)[0], returnedPerson.getLastName());
+        assertEquals(sampleMap.get(PersonLogic.PHONE)[0], returnedPerson.getPhone());
+        assertEquals(sampleMap.get(PersonLogic.ADDRESS)[0], returnedPerson.getAddress());
+        assertEquals(sampleMap.get(PersonLogic.BIRTH)[0], logic.convertDateToString(returnedPerson.getBirth()));
+
+        sampleMap = new HashMap<>();
+        sampleMap.put(PersonLogic.ID, new String[]{Integer.toString(1)});
+        sampleMap.put(PersonLogic.FIRST_NAME, new String[]{generateString.apply(50)});
+        sampleMap.put(PersonLogic.LAST_NAME, new String[]{generateString.apply(50)});
+        sampleMap.put(PersonLogic.PHONE, new String[]{generateString.apply(15)});
+        sampleMap.put(PersonLogic.ADDRESS, new String[]{generateString.apply(100)});
+        sampleMap.put(PersonLogic.BIRTH, new String[]{logic.convertDateToString(expectedEntity.getBirth())});
+
+        //idealy every test should be in its own method
+        returnedPerson = logic.createEntity(sampleMap);
+        assertEquals(Integer.parseInt(sampleMap.get(PersonLogic.ID)[0]), returnedPerson.getId());
+        assertEquals(sampleMap.get(PersonLogic.FIRST_NAME)[0], returnedPerson.getFirstName());
+        assertEquals(sampleMap.get(PersonLogic.LAST_NAME)[0], returnedPerson.getLastName());
+        assertEquals(sampleMap.get(PersonLogic.PHONE)[0], returnedPerson.getPhone());
+        assertEquals(sampleMap.get(PersonLogic.ADDRESS)[0], returnedPerson.getAddress());
+        assertEquals(sampleMap.get(PersonLogic.BIRTH)[0], logic.convertDateToString(returnedPerson.getBirth()));
     }
     
     @Test
