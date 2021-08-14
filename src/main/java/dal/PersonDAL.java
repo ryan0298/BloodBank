7af53e8350552c662101a1f3a0602813cfed946f
aver0042 <84288027+aver0042@users.noreply.github.com>
2021-08-13 20:19:58 -0400

    Finalizes and cleans all Person-related files
    Comments, code cleanup, etc.

--- src/main/java/dal/PersonDAL.java
+++ src/main/java/dal/PersonDAL.java
@@ -7,53 +7,96 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * DAL entity for the Person entity.
+ *
  * @author Jack Avery
  */
 public class PersonDAL extends GenericDAL<Person> {
-    
+
     public PersonDAL() {
         super(Person.class);
     }
-    
+
+    /**
+     * Returns a list containing every Person.
+     *
+     * @return List<Person> - All Person entities in the database.
+     */
     @Override
     public List<Person> findAll() {
         return findResults("Person.findAll", null);
     }
-    
+
+    /**
+     * Returns the person with the given ID.
+     *
+     * @param id int - The ID of the Person
+     * @return Person - The Person with the given ID
+     */
     @Override
     public Person findById(int personId) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "id", personId);
+        map.put("id", personId);
         return findResult("Person.findById", map);
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given First Name.
+     *
+     * @param firstName String - The First Name to filter by.
+     * @return List<Person> - All Persons with the given First Name.
+     */
     public List<Person> findByFirstName(String firstName) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "firstName", firstName);
+        map.put("firstName", firstName);
         return findResults("Person.findByFirstName", map);
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Last Name.
+     *
+     * @param lastName String - The Last Name to filter by.
+     * @return List<Person> - All Persons with the given Last Name.
+     */
     public List<Person> findByLastName(String lastName) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "lastName", lastName);
+        map.put("lastName", lastName);
         return findResults("Person.findByLastName", map);
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Phone number.
+     *
+     * @param phone String - The Phone number to filter by.
+     * @return List<Person> - All Persons with the given Phone number.
+     */
     public List<Person> findByPhone(String phone) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "phone", phone);
+        map.put("phone", phone);
         return findResults("Person.findByPhone", map);
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Address.
+     *
+     * @param address String - The Address to filter by.
+     * @return List<Person> - All Persons with the given Address.
+     */
     public List<Person> findByAddress(String address) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "address", address);
+        map.put("address", address);
         return findResults("Person.findByAddress", map);
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Birth Date.
+     *
+     * @param birth Date - The Birth Date to filter by.
+     * @return List<Person> - All Persons with the given Birth Date.
+     */
     public List<Person> findByBirth(Date birth) {
         Map<String, Object> map = new HashMap<>();
-        map.put( "birth", birth);
+        map.put("birth", birth);
         return findResults("Person.findByBirth", map);
     }
 }
--- src/main/java/logic/PersonLogic.java
+++ src/main/java/logic/PersonLogic.java
@@ -1,75 +1,136 @@
 package logic;
 
 import common.ValidationException;
-import dal.DonationRecordDAL;
 import dal.PersonDAL;
-import entity.BloodBank;
-import entity.DonationRecord;
 import entity.Person;
-import entity.RhesusFactor;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Arrays;
 
 import java.util.List;
 import java.util.Date;
 import java.util.Map;
 import java.util.Objects;
-import java.util.Set;
 import java.util.function.ObjIntConsumer;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
+ * Logic holder for the Person entity.
+ *
  * @author Jack Avery
  * @author Ryanh
  * @author Milad Mobini
  */
-public class PersonLogic extends GenericLogic<Person, PersonDAL>  {
-    
+public class PersonLogic extends GenericLogic<Person, PersonDAL> {
+
+    /**
+     * Stores the first_name column name space.
+     */
     public static final String FIRST_NAME = "first_name";
+    /**
+     * Stores the last_name column name space.
+     */
     public static final String LAST_NAME = "last_name";
+    /**
+     * Stores the phone column name space.
+     */
     public static final String PHONE = "phone";
+    /**
+     * Stores the address column name space.
+     */
     public static final String ADDRESS = "address";
+    /**
+     * Stores the birth column name space.
+     */
     public static final String BIRTH = "birth";
-    public static final String BLOODBANK_ID = "bloodbank_id";
+    /**
+     * Stores the id column name space.
+     */
     public static final String ID = "person_id";
-    
+    /**
+     * Stores the name space for the Blood Bank ID.
+     */
+    public static final String BLOODBANK_ID = "bloodbank_id";
+
     PersonLogic() {
         super(new PersonDAL());
     }
 
+    /**
+     * Returns a list containing every Person.
+     *
+     * @return List<Person> - All Person entities in the database.
+     */
     @Override
     public List<Person> getAll() {
         return get(() -> dal().findAll());
     }
 
+    /**
+     * Returns the person with the given ID.
+     *
+     * @param id int - The ID of the Person
+     * @return Person - The Person with the given ID
+     */
     @Override
     public Person getWithId(int id) {
         return get(() -> dal().findById(id));
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given First Name.
+     *
+     * @param firstName String - The First Name to filter by.
+     * @return List<Person> - All Persons with the given First Name.
+     */
     public List<Person> getPersonWithFirstName(String firstName) {
         return get(() -> dal().findByFirstName(firstName));
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Last Name.
+     *
+     * @param lastName String - The Last Name to filter by.
+     * @return List<Person> - All Persons with the given Last Name.
+     */
     public List<Person> getPersonWithLastName(String lastName) {
         return get(() -> dal().findByLastName(lastName));
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Phone number.
+     *
+     * @param phone String - The Phone number to filter by.
+     * @return List<Person> - All Persons with the given Phone number.
+     */
     public List<Person> getPersonWithPhone(String phone) {
         return get(() -> dal().findByPhone(phone));
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Address.
+     *
+     * @param address String - The Address to filter by.
+     * @return List<Person> - All Persons with the given Address.
+     */
     public List<Person> getPersonWithAddress(String address) {
         return get(() -> dal().findByAddress(address));
     }
-    
+
+    /**
+     * Returns a list of all Persons with the given Birth Date.
+     *
+     * @param birth Date - The Birth Date to filter by.
+     * @return List<Person> - All Persons with the given Birth Date.
+     */
     public List<Person> getPersonWithBirth(Date birth) {
         return get(() -> dal().findByBirth(birth));
     }
-    
+
+    /**
+     * Given a map (key, value(s)) of parameters, creates a new Person.
+     *
+     * @param parameterMap Map<String, String[]> - The parameters for the new
+     * Person.
+     * @return Person - The new Person entity.
+     */
     @Override
     public Person createEntity(Map<String, String[]> parameterMap) {
         Objects.requireNonNull(parameterMap, "parameterMap cannot be null");
@@ -103,36 +164,52 @@ public class PersonLogic extends GenericLogic<Person, PersonDAL>  {
         String address = parameterMap.get(ADDRESS)[0];
         String birth = parameterMap.get(BIRTH)[0];
         birth = birth.replace("T", " "); //From Ryan's code
-        
+
         validator.accept(firstName, 50);
         validator.accept(lastName, 50);
         validator.accept(phone, 15);
         validator.accept(address, 100);
         validator.accept(birth, 45);
-        
+
         entity.setFirstName(firstName);
         entity.setLastName(lastName);
         entity.setPhone(phone);
         entity.setAddress(address);
-        
+
         try {
             entity.setBirth(convertStringToDate(birth));
         } catch (ValidationException ex) {
             entity.setBirth(new Date());
-        }        
+        }
         return entity;
     }
-    
+
+    /**
+     * Returns a descriptive list of the Database columns for Person.
+     *
+     * @return List<String> - Formal names of the database columns.
+     */
     @Override
     public List<String> getColumnNames() {
         return Arrays.asList("ID", "First Name", "Last Name", "Phone Number", "Address", "Date of Birth");
     }
 
+    /**
+     * Returns a list containing the column codes for Person.
+     *
+     * @return List<String> - The column codes for Person.
+     */
     @Override
     public List<String> getColumnCodes() {
         return Arrays.asList(ID, FIRST_NAME, LAST_NAME, PHONE, ADDRESS, BIRTH);
     }
 
+    /**
+     * Returns a list containing stored data for a Person.
+     *
+     * @param e Person - The Person to extract data from.
+     * @return List<?> - The Persons' data, ordered in a list.
+     */
     @Override
     public List<?> extractDataAsList(Person e) {
         return Arrays.asList(
@@ -145,5 +222,5 @@ public class PersonLogic extends GenericLogic<Person, PersonDAL>  {
                 e.getBloodBank(),
                 e.getDonationRecordSet());
     }
-    
+
 }
